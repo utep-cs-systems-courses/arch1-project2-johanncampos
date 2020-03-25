1487980faae0fea3199d0120603137457a4bb17c
johanncampos <jscampos@miners.utep.edu>
2020-03-24 19:42:12 -0600

    included buzzer.h

--- project/toy_project/toyMain.c
+++ project/toy_project/toyMain.c
@@ -2,11 +2,14 @@
 #include "libTimer.h"
 #include "led.h"
 #include "switches.h"
+#include "buzzer.h"
 
 int main(void) {
   configureClocks();
+  
   switch_init();
   led_init();
+  buzzer_init();
   enableWDTInterrupts();
 
   or_sr(0x18);
