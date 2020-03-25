9ee833e1b2e34b85715e2b1aeb4b0aeb036150c7
johanncampos <jscampos@miners.utep.edu>
2020-03-24 19:47:32 -0600

    removed led_changed variable

--- project/toy_project/led.c
+++ project/toy_project/led.c
@@ -2,24 +2,18 @@
 #include "led.h"
 
 unsigned char red_on = 0, green_on = 0;
-unsigned char led_changed = 0;
 
 void led_init() {
   P1DIR = LEDS;
-  led_changed = 1;
   led_update();
 }
 
 void led_update() {
-  if (led_changed) {
-    char ledFlags = 0;
+  char ledFlags = 0;
+  
+  if (red_on) {ledFlags |= LED_RED;}
+  if (green_on) {ledFlags |= LED_GREEN;}
 
-    if (red_on) {ledFlags |= LED_RED;}
-    if (green_on) {ledFlags |= LED_GREEN;}
-
-    P1OUT &= (0xff^LEDS) | ledFlags;
-    P1OUT |= ledFlags;
-
-    led_changed = 0;
-  }
+  P1OUT &= (0xff^LEDS) | ledFlags;
+  P1OUT |= ledFlags;
 }
